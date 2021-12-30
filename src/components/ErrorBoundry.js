87e4a9a9ebf7582d5e9bcc20fb28e8a6643974c3
Vivadhyay <vivekupadhyay.92@gmail.com>
2021-12-30 22:04:44 +0530

    app using react

---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class ErrorBoundry extends Component {
+    constructor (props){
+        super(props);
+        this.state = {
+            haserror: false
+        }
+    }
+componentDidCatch(error, info) {
+    this.setstate({haserror: true})
+}
+render() {
+    if(this.state.haserror) {
+        return <h1>Oooops.This is not good</h1>
+    }
+    return this.props.children
+}
+};
+
+export default ErrorBoundry;
---
+++ src/components/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'HariHar',
+    username: 'HH',
+    email: 'harry@yahoo.com'
+  },
+  {
+    id: 2,
+    name: 'Shyam',
+    username: 'Sha',
+    email: 'Shhyam@reddit.tv'
+  },
+  {
+    id: 3,
+    name: 'Sawariya',
+    username: 'Sawa',
+    email: 'sawariya@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Rahul kumar',
+    username: 'rah',
+    email: 'rahul@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'Rajat Pachouri',
+    username: 'raj',
+    email: 'rajat@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'Ram Dixit',
+    username: 'ram',
+    email: 'ram@yahoo.com'
+  },
+  {
+    id: 7,
+    name: 'Manish Kochar',
+    username: 'Man',
+    email: 'manish@gmail.com'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Cage',
+    username: 'nick',
+    email: 'nicholas@hollywood.us'
+  },
+  {
+    id: 9,
+    name: 'Akshay',
+    username: 'ak',
+    email: 'akshay@bollywood.in'
+  },
+  {
+    id: 10,
+    name: 'Narendra Modi',
+    username: 'modi',
+    email: 'nmodi@india.gov.in'
+  }
+];
